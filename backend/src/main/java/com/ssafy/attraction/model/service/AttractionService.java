57ea3b2669ad0fb948aa427ef555e279df416079
lkt9899 <lkt9899@naver.com>
2023-11-14 19:04:23 +0900

    feat : add attraction service

--- backend/src/main/java/com/ssafy/attraction/model/service/AttractionService.java
+++ backend/src/main/java/com/ssafy/attraction/model/service/AttractionService.java
@@ -2,6 +2,7 @@ package com.ssafy.attraction.model.service;
 
 import com.ssafy.attraction.model.dao.AttractionRepository;
 import com.ssafy.attraction.model.dto.Attraction;
+import com.ssafy.attraction.model.dto.SearchCondition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,8 +17,8 @@ public class AttractionService {
 
    private final AttractionRepository mapper;
 
-   public List<Attraction> selectAll() throws SQLException{
-      return mapper.selectAll();
+   public List<Attraction> select(SearchCondition condition) throws SQLException{
+      return mapper.select(condition);
    }
 
 }
