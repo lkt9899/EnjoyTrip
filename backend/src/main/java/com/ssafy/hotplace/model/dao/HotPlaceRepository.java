5b2371065d45e5dc53bd748cab5543a12319cf81
lkt9899 <lkt9899@naver.com>
2023-11-15 17:12:53 +0900

    feat : add hotplace service

--- backend/src/main/java/com/ssafy/hotplace/model/dao/HotPlaceRepository.java
+++ backend/src/main/java/com/ssafy/hotplace/model/dao/HotPlaceRepository.java
@@ -1,12 +1,17 @@
 package com.ssafy.hotplace.model.dao;
 
-import com.ssafy.hotplace.model.dto.HotPlace;
+import com.ssafy.hotplace.model.dto.data.common.HotPlace;
+import com.ssafy.hotplace.model.dto.data.request.HotPlaceRequest;
+import com.ssafy.hotplace.model.dto.data.response.HotPlaceResponse;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.SQLException;
 
 @Mapper
 public interface HotPlaceRepository {
-    void insert(HotPlace hotplace) throws SQLException;
+    void insert(HotPlaceRequest req) throws SQLException;
     void update(HotPlace hotplace) throws SQLException;
+    HotPlaceResponse select(int hotPlaceId) throws SQLException;
+
+    void updateHit(int hotPlaceId) throws SQLException;
 }
--- backend/src/main/java/com/ssafy/hotplace/model/service/HotPlaceService.java
+++ backend/src/main/java/com/ssafy/hotplace/model/service/HotPlaceService.java
@@ -1,11 +1,34 @@
 package com.ssafy.hotplace.model.service;
 
-import com.ssafy.hotplace.model.dto.HotPlace;
+import com.ssafy.hotplace.model.dao.HotPlaceRepository;
+import com.ssafy.hotplace.model.dto.data.common.HotPlace;
+import com.ssafy.hotplace.model.dto.data.request.HotPlaceRequest;
+import com.ssafy.hotplace.model.dto.data.response.HotPlaceResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class HotPlaceService {
+    private final HotPlaceRepository mapper;
 
-public interface HotPlaceService {
-    void insert(HotPlace hotplace) throws SQLException;
-    void update(HotPlace hotplace) throws SQLException;
+    @Transactional
+    public void insert(HotPlaceRequest req) throws SQLException {
+        mapper.insert(req);
+    }
+
+    @Transactional
+    public void update(HotPlaceRequest req) throws SQLException {
+        mapper.update(req);
+    }
+
+    @Transactional
+    public HotPlaceResponse select(int hotPlaceId) throws SQLException {
+        mapper.updateHit(hotPlaceId);
+        return mapper.select(hotPlaceId);
+    }
 }
