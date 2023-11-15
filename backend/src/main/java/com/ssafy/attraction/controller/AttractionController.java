678922adebf053d864994cc2597e44d9ed068376
Jiwon Han <gkswldnj0803@gmail.com>
2023-11-16 01:47:11 +0900

    add cors annotation to  attracion controller

--- backend/src/main/java/com/ssafy/attraction/controller/AttractionController.java
+++ backend/src/main/java/com/ssafy/attraction/controller/AttractionController.java
@@ -7,14 +7,17 @@ import com.ssafy.attraction.model.service.AttractionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
 import java.util.List;
 
+@CrossOrigin(origins = { "*" }, methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.POST} , maxAge = 6000)
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/attraction")
