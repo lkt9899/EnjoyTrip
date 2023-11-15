4104dd5981494b1bf75cd41539816e96ae3e7afa
lkt9899 <lkt9899@naver.com>
2023-11-15 21:22:49 +0900

    feat : add member response dto

---
+++ backend/src/main/java/com/ssafy/member/model/dto/data/response/MemberLoginResponse.java
@@ -0,0 +1,11 @@
+package com.ssafy.member.model.dto.data.response;
+
+import com.ssafy.member.model.dto.data.common.MemberLogin;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class MemberLoginResponse extends MemberLogin {
+    private int memberId;
+}
