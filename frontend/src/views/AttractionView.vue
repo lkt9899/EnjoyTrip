541978a9a931272a2ae6cf58caa28a3dd54a5c70
lkt9899 <lkt9899@naver.com>
2023-11-20 14:14:41 +0900

    feat : init attraction view

---
+++ frontend/src/views/AttractionView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NavBar from "../components/layout/NavBar.vue";
+import MyFooter from "../components/layout/MyFooter.vue";
+</script>
+
+<template>
+    <div class="container">
+        <NavBar />
+
+        <MyFooter />
+    </div>
+</template>
+
+<style scoped></style>
