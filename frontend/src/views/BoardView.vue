ef5dd37174ed3817da9089a96eaad4c2e0fc117c
Jiwon Han <gkswldnj0803@gmail.com>
2023-11-21 10:15:26 +0900

    refactor: router-view 한 개만 갖도록 구조 수정, feat BoardView 백앤드 연동

---
+++ frontend/src/views/BoardView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import BoardList from '../components/board/BoardList.vue';
+import NavBar from "../components/layout/NavBar.vue";
+</script>
+
+<template>
+  <NavBar></NavBar>
+  <div class="container text-center mt-3">
+    <div class="alert alert-info" role="alert">자유롭게 글쓰는 공간이당</div>
+    <BoardList></BoardList>
+  </div>
+  
+</template>
+
+<style>
+mark.sky {
+  background: linear-gradient(to top, #54fff9 20%, transparent 30%);
+}
+</style>
