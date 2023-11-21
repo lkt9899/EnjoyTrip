a6e23eab13742ec6aff8fba0387e348679822e17
Jiwon Han <gkswldnj0803@gmail.com>
2023-11-21 10:11:30 +0900

    router: 자식 컴포넌트에서 router-view를 갖는 구조에 맞게 수정

---
+++ frontend/src/views/board/BoardView.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <div class="container text-center mt-3">
+    Hello, I am BoardView.
+    <div class="alert alert-info" role="alert">자유롭게 글쓰는 공간</div>
+
+  </div>
+  
+</template>
+
+<style>
+mark.sky {
+  background: linear-gradient(to top, #54fff9 20%, transparent 30%);
+}
+</style>
