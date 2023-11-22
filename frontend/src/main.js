04e4c3a39baebcd171388095b8164b5647344d82
lkt9899 <lkt9899@naver.com>
2023-11-22 12:20:55 +0900

    style : add quasar plugin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { Quasar } from 'quasar';
+import quasarLang from 'quasar/lang/ko-KR';
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css';
+
+// Import Quasar css
+import 'quasar/src/css/index.sass';
 
 import App from './App.vue';
 import router from './router';
@@ -14,5 +22,9 @@ const app = createApp(App);
 // router & pinia
 app.use(createPinia());
 app.use(router);
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+    lang: quasarLang,
+});
 
 app.mount('#app');
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,21 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue({
+            template: { transformAssetUrls },
+        }),
+
+        // @quasar/plugin-vite options list:
+        // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+        quasar({
+            sassVariables: 'src/css/quasar-variables.sass',
+        }),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
