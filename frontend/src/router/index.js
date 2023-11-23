960c9be0e18c7d58aa86e5bd0f84333a2dd27bc6
lkt9899 <lkt9899@naver.com>
2023-11-24 02:46:40 +0900

    fix : view name change

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,7 +16,7 @@ const router = createRouter({
         {
             path: '/login',
             name: 'login',
-            component: () => import('@/views/Login.vue'),
+            component: () => import('@/views/LoginView.vue'),
         },
         {
             path: '/board',
