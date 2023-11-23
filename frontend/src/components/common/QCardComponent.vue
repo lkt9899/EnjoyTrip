8be199fb83e4c9d93ebfd55aa0f3673146807d43
lkt9899 <lkt9899@naver.com>
2023-11-23 15:13:24 +0900

    feat : add QCard type

--- frontend/src/components/common/QCardComponent.vue
+++ frontend/src/components/common/QCardComponent.vue
@@ -1,12 +1,12 @@
 <script setup>
 defineProps({
     item: Object,
-    altImg: String
+    type: String
 });
 </script>
 
 <template>
-    <q-card class="my-card px-1">
+    <q-card v-if="type == 'at'" class="my-card px-1">
         <q-card-section class="p-1 my-1">
             <q-img v-if="item.firstImage != ''" :src="item.firstImage" />
             <q-img v-else src="@/assets/noImage.jpg" />
@@ -14,6 +14,15 @@ defineProps({
         <q-card-section>
             <div class="text-h6">{{ item.title }}</div>
             <div class="text-subtitle2">{{ item.addr1 }}</div>
+        </q-card-section>
+    </q-card>
+    <q-card v-else class="my-card px-1">
+        <q-card-section>
+            <div class="text-h6">{{ item.atitle }}</div>
+            <div class="d-flex justify-content-between text-subtitle2">
+                <span>{{ item.title }}</span>
+                <span>{{ item.name }}</span>
+            </div>
         </q-card-section>
     </q-card>
 </template>
